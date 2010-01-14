3dff9971eaf9ec4802f1fcbe35bcc93222d0383d
Trenton Lipscomb <trentonl@gmail.com>
2010-01-13 19:31:55 -0800

    initial revision

---
+++ src/test/scala/com/runbam/testoftests/UtilJunitTest.scala
@@ -0,0 +1,33 @@
+package com.runbam.testoftests
+
+import org.junit._
+import Assert._
+import org.hamcrest.Matchers._
+import org.junit.Test
+
+
+@Test
+class UtilJunitTest {
+  @Test
+  def testReflect = {
+    assertThat(Util.reflect(5), is(5))
+    assertThat(Util.reflect("xyz"), is("xyz"))
+    assertThat(Util.reflect(1234.56), is(1234.56))
+  }
+
+  @Test
+  def testNegate = {
+    assertEquals(Util.negate(1), -1)
+    assertEquals(Util.negate(-1), 1)
+    assertEquals(Util.negate(1.2f), -1.2f)
+
+    try {
+      assertEquals(Util.negate('x'), 'X')
+      fail("Should have thrown exception for unknown type")
+    }
+    catch {
+      case e: IllegalArgumentException => assertThat(e.getMessage, startsWith("Don't know how to negate "))
+      case e => fail("Caught unexpected exception: " + e.getMessage)
+    }
+  }
+}
