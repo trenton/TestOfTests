727fc26299f26ae85b49f69d53751c88eec014a3
Trenton Lipscomb <trentonl@gmail.com>
2010-01-15 12:38:48 +0800

    tests for property values

--- src/main/scala/com/runbam/testoftests/Tweet.scala
+++ src/main/scala/com/runbam/testoftests/Tweet.scala
@@ -3,10 +3,10 @@ package com.runbam.testoftests
 
 class Tweet(val author: String, val body: String) {
   // max message size is 140
-  require(body.length <= Tweet.MAX_BODY_LENGTH)
+  require(body.length <= Tweet.MAX_BODY_LENGTH, "body too big")
 
   // do not allow empty messages
-  require(body.length > 0)
+  require(body.length > 0, "body empty")
 
   override def toString = "@%s: %s".format(author, body)
 }
--- src/test/scala/com/runbam/testoftests/TwitterTest.scala
+++ src/test/scala/com/runbam/testoftests/TwitterTest.scala
@@ -9,10 +9,29 @@ import org.junit.Test
 @Test
 class TwitterTest {
   @Test
-  def testTweetCreate = {
+  def testTweetCreate: Unit = {
     val t1 = new Tweet("author", "message")
     assertThat(t1.author, is("author"))
     assertThat(t1.body, is("message"))
     assertThat(t1.toString, is("@author: message"))
+
+    val bigMessage = (0 until Tweet.MAX_BODY_LENGTH + 1).foldLeft("")(_ + _)
+    try {
+      new Tweet("author", bigMessage)
+      fail("Long message should bugger us")
+    }
+    catch {
+      case e: IllegalArgumentException => assertTrue(true)
+      case _ => fail("Long message should bugger us")
+    }
+
+    try {
+      new Tweet("author", "")
+      fail("Empty message should bugger us")
+    }
+    catch {
+      case e: IllegalArgumentException => assertTrue(true)
+      case _ => fail("Empty message should bugger us")
+    }
   }
 }
