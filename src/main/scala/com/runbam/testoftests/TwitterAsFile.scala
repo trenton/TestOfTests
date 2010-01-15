514241eaec1efa4fc004c23383dfb98febcc638a
Trenton Lipscomb <trentonl@gmail.com>
2010-01-15 13:09:41 +0800

    spool-to-disk Twitter impl + tests

---
+++ src/main/scala/com/runbam/testoftests/TwitterAsFile.scala
@@ -0,0 +1,17 @@
+package com.runbam.testoftests
+
+
+import java.io.{FileWriter, File}
+
+class TwitterAsFile(out: File) extends Twitter {
+  def getFeed(start: Int, end: Int) = {
+    throw new UnsupportedOperationException
+  }
+
+  def post(post: Tweet) = {
+    val writer: FileWriter = new FileWriter(out)
+    writer.write(post.toString)
+    writer.write("\n")
+    writer.close
+  }
+}
