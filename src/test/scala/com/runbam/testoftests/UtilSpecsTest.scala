d75679ca455a4d728a5eaad50df6806ec12841cb
Trenton Lipscomb <trentonl@gmail.com>
2010-01-20 15:36:46 -0800

    added actual tests

--- src/test/scala/com/runbam/testoftests/UtilSpecsTest.scala
+++ src/test/scala/com/runbam/testoftests/UtilSpecsTest.scala
@@ -17,4 +17,20 @@ object UtilSpec extends Specification {
   "'hello world' matches 'h.* w.*'" in {
     "hello world" must be matching ("h.* w.*")
   }
+
+  "negate(0) is 0" in {
+    Util.negate(0) must_== 0
+  }
+
+  "negate(1) is -1" in {
+    Util.negate(1) must_== -1
+  }
+
+  "negate(negate(n)) is n" in {
+    val rand = new Random
+
+    val n = rand.nextInt(2^31 - 1)
+    Util.negate(n) must_== n * -1
+    Util.negate(Util.negate(n)) must_== n
+  }
 }
