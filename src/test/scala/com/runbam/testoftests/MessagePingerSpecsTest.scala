68f8dece1938420296800adaf0601c9164970c10
Trenton Lipscomb <trentonl@gmail.com>
2010-01-21 12:20:07 -0800

    added inline example of a matcher

--- src/test/scala/com/runbam/testoftests/MessagePingerSpecsTest.scala
+++ src/test/scala/com/runbam/testoftests/MessagePingerSpecsTest.scala
@@ -8,7 +8,7 @@ import org.specs.Specification
 import org.hamcrest.Matchers._
 import org.junit.Assert._
 
-class MessagePingerSpecsTest extends JUnit4(MessagePingerSpec)
+class MessagePingerSpecsTest extends JUnit4(MessagePingerSpec, MessagePingerSpec2)
 
 case class MessageMatcher(val expectedAuthor: String) extends Matcher[Message] {
   // :TODO: understand =>Class syntax
@@ -19,7 +19,6 @@ case class MessageMatcher(val expectedAuthor: String) extends Matcher[Message] {
   }
 }
 
-// http://code.google.com/p/specs/wiki/UsingJMock
 object MessagePingerSpec extends Specification with JMocker with ClassMocker {
   "MessagePinger" should {
     "use mocks" in {
@@ -30,6 +29,37 @@ object MessagePingerSpec extends Specification with JMocker with ClassMocker {
         // :TODO: message on failure says post failed to be called, not that the matcher failed
         // :TODO: can this be done without the helper class?
         exactly(2).of(msgService).post(new MessageMatcher(MessagePinger.AUTHOR))
+      }
+
+      val delayMs = 2000
+      val pinger = new MessagePinger(delayMs, msgService)
+      pinger.start
+
+      Thread.sleep(delayMs + 100)
+
+      pinger.shutdown
+    }
+  }
+}
+
+// http://code.google.com/p/specs/wiki/UsingJMock
+object MessagePingerInlineMatcherSpec extends Specification with JMocker with ClassMocker {
+  "MessagePinger" should {
+    "use mocks" in {
+      val msgService = mock[MessageService]
+
+      expect {
+        val messageCapture = capturingParam[Message]
+        // :TODO: message on failure says post failed to be called, not that the matcher failed
+        // :TODO: can this be done without the helper class?
+        exactly(2).of(msgService).post(new Matcher[Message] {
+          def apply(msg: =>Message) = {
+            val expectedAuthor = MessagePinger.AUTHOR
+            val success = "expected %s and saw %s".format(expectedAuthor, msg.author)
+            val failure = "expected %s but saw %s".format(expectedAuthor, msg.author)
+            (msg.author == expectedAuthor, success, failure)
+          }
+        })
       }
 
       val delayMs = 2000
