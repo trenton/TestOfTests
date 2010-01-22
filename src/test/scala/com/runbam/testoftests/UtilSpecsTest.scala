2e299853d106eb3babd99260a25b6894a6f58a85
Trenton Lipscomb <trentonl@gmail.com>
2010-01-21 17:32:34 -0800

    added DataTables example

--- src/test/scala/com/runbam/testoftests/UtilSpecsTest.scala
+++ src/test/scala/com/runbam/testoftests/UtilSpecsTest.scala
@@ -4,21 +4,14 @@ import org.junit.Test
 import org.specs._
 import runner.JUnit4
 import org.scalacheck.Prop.forAll
+import util.DataTables
 
 /**
  * http://code.google.com/p/specs/wiki/RunningSpecs
  */
-class UtilSpecsTest extends JUnit4(UtilSpec)
+class UtilSpecsTest extends JUnit4(UtilSpec, UtilAdvancedSpec)
 
 object UtilSpec extends Specification {
-  "'hello world' has 11 characters" in {
-    "hello world".size must_== 11
-  }
-
-  "'hello world' matches 'h.* w.*'" in {
-    "hello world" must be matching ("h.* w.*")
-  }
-
   "negate(0) is 0" in {
     Util.negate(0) must_== 0
   }
@@ -47,5 +40,20 @@ object UtilSpec extends Specification {
     Util.repeat("foo", 0) must be matching("")
     Util.repeat("foo", 3) must be matching("^foofoofoo$")
     Util.repeat("foo", 3) must have size ("foo".size * 3)
+  }
+}
+
+// http://code.google.com/p/specs/wiki/AdvancedSpecifications
+// :TODO: not the best error message on failure, despite what docs show should happen
+object UtilAdvancedSpec extends Specification with DataTables {
+  "repeated repeat tests" in {
+    "string" | "times" | "expected" |>
+    "foo"    ! 0       ! ""         |
+    "foo"    ! 1       ! "foo"      |
+    "foo"    ! 2       ! "foofoo"   |
+    "foo "   ! 2       ! "foo foo " | 
+            {(str, times, expectation) =>
+      Util.repeat(str, times) must be equalTo(expectation)
+    }
   }
 }
